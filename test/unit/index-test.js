5b32eefedeb98808c27224d448f486fe5b422d18
kanekotic <alvarojosepl@gmail.com>
2018-09-19 08:55:22 +0200

    initial commit

---
+++ test/unit/index-test.js
@@ -0,0 +1,8 @@
+// jest.mock('../../lib/oauth1-login', () => jest.fn())
+const //loginOauth1 = require('../../src/oauth1-login'),
+    index = require('../../src/index')
+
+describe('index should', () => {
+    test('export load functionality', async () => {
+    })
+})
