b3e9343d7f9ca4a4f4648942ead6cbc9de64de38
kanekotic <alvarojosepl@gmail.com>
2018-09-28 09:46:53 +0200

    added integration tests

---
+++ test/integration/integration-test.js
@@ -0,0 +1,13 @@
+const index = require('../../src/index'),
+    faker = require('faker')
+
+let config = { 
+    installPath: `${__dirname}/test_folder`,
+    plugins : {
+        "electron-plugin-example": "git+https://git@github.com:kanekotic/electron-plugin-example.git"
+    }
+ },
+extensionPoint = {
+    app: faker.random.uuid()
+}
+index.load(config, extensionPoint).then(console.log).catch((error) => {throw error})
