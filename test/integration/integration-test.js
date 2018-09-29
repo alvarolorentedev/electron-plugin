57f381e290ed17a624d718c8d86356dd52268c08
kanekotic <alvarojosepl@gmail.com>
2018-09-29 13:03:44 +0200

    added tape for integration

--- test/integration/integration-test.js
+++ test/integration/integration-test.js
@@ -1,13 +1,19 @@
-const index = require('../../src/index'),
-    faker = require('faker')
+let test = require('tape'),
+    faker = require('faker'),
+    app = require('../../src/index')
 
-let config = { 
-    installPath: `${__dirname}/test_folder`,
-    plugins : {
-        "electron-plugin-example": "git+ssh://git@github.com/kanekotic/electron-plugin-example.git"
+test('integration should download and install', async (assert) => {
+    assert.plan(1);
+    let config = { 
+        installPath: `${__dirname}/test_folder`,
+        plugins : {
+            "electron-plugin-example": "git+ssh://git@github.com/kanekotic/electron-plugin-example.git"
+        }
+     },
+    extensionPoint = {
+        app: faker.random.uuid()
     }
- },
-extensionPoint = {
-    app: faker.random.uuid()
-}
-index.load(config, extensionPoint).then(console.log).catch((error) => {throw error})
+    await app.load(config, extensionPoint)
+    assert.pass()
+    assert.end();
+  });
